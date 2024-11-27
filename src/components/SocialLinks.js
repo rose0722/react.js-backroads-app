9bc67c6bbd3f347d82df413ba9ffdf81c14859fd
Rose <liene@g-angle.co.jp>
2024-11-27 15:41:59 +0800

    fix error

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialLinks = ({ href, icon, itemclass }) => {
   return (
     <li>
-        <a href={href} target="_blank" className={itemclass}>
+        <a href={href} target="_blank" rel="noopener noreferrer" className={itemclass}>
         <i className={icon}></i></a>
     </li>
   )
